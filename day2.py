c0fce87aa906aad4e79ef43124572e5ea60b01af
Fireball19 <fireball1199@googlemail.com>
2022-07-17 23:06:10 +0200

    Add day2

---
+++ day2.py
@@ -0,0 +1,16 @@
+horizontal = 0
+depth = 0
+
+with open('day2_input.txt') as f:
+    for line in f:
+        command = line.split()[0]
+        value = int(line.split()[1])
+        if command == "forward":
+            horizontal += value
+        elif command == "up":
+            depth -= value
+        elif command == "down":
+            depth += value
+
+print(horizontal * depth)
+f.close()
