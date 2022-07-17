af55f0edb24d9fe8f95c879cc451e0b60f31d1f5
Fireball19 <fireball1199@googlemail.com>
2022-07-17 22:51:38 +0200

    Add day1

---
+++ day1.py
@@ -0,0 +1,12 @@
+count = -1
+previous = -1
+
+with open('day1_input.txt') as f:
+    for line in f:
+        tmp = int(line)
+        if tmp > previous:
+            count += 1
+        previous = tmp
+
+print(count)
+f.close()
